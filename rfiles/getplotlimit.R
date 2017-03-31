e1ace5b29f8ab62285fa784873a94e0612cfd713
wildwolflin <weiting.lin.lin@gmail.com>
2017-03-31 17:45:23 -0500

    fix getplotlimit.R

--- rfiles/getplotlimit.R
+++ rfiles/getplotlimit.R
@@ -10,9 +10,9 @@ getPlotLimit <- function(vennout, adj = 1.5){
   r3 <- vennout$r3
   
   xmax <- max(c1[1] + r1, c2[1] + r2, c3[1], + r3)
-  xmin <- min(c1[1] + r1, c2[1] + r2, c3[1], + r3)
+  xmin <- min(c1[1] - r1, c2[1] - r2, c3[1], - r3)
   ymax <- max(c1[2] + r1, c2[2] + r2, c3[2], + r3)
-  ymin <- min(c1[2] + r1, c2[2] + r2, c3[2], + r3)
+  ymin <- min(c1[2] - r1, c2[2] - r2, c3[2], - r3)
   
   size <- max( xmax - xmin, ymax - ymin) * adj #¡@the longer one
   
--- rfiles/plotVenn.R
+++ rfiles/plotVenn.R
@@ -1,6 +1,7 @@
 
 source("plotcircle.R")
 source("getTriangle.R")
+source("getplotlimit.R")
 
 plotVenn <- function(venn, 
                      color = list(rgb(0,0,1,0.3),rgb(1,0, 0, 0.3), rgb(0, 1, 0, 0.3)),
@@ -9,6 +10,8 @@ plotVenn <- function(venn,
   
   plot(1, type="n", xlim=c(-3, 5), ylim=c(-5, 3),  
        xaxt = 'n', ann = FALSE, yaxt ='n')
+  
+  
   
   plotCircle(venn$c1, venn$r1, color[[1]], line = line, lwd = lwd)
   plotCircle(venn$c2, venn$r2, color[[2]], line = line, lwd = lwd)

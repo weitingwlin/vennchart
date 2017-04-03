19b8bd1a171f4e8d2c1e2aacd935c6bc7bcb3ed9
wildwolflin <weiting.lin.lin@gmail.com>
2017-04-03 08:11:37 -0500

    Add: vennchart.R

---
+++ rfiles/vennchart.R
@@ -0,0 +1,27 @@
+#source("plotvenn.R")
+
+vennChart <- function(A_all , B_all , C_all ,
+                      AandB , AandC , BandC,
+                      plot = TRUE, 
+                      colors = list(rgb(0,0,1,0.3), rgb(1,0, 0, 0.3), rgb(0, 1, 0, 0.3)),
+                      frameadj = 1.5, line  = NA, lwd = 2, 
+                      texts = TRUE, names = c("A", "B", "C"),
+                      Value = TRUE, cex = 1, textadj = c(0.5, 0.5),
+                      connect = "\n", unit = " "){
+    
+    
+    s <- vennThreeCircle(A_all , B_all , C_all , AandB , AandC , BandC )
+    p <- vennTextPosition(s, textadj)
+    
+    out <- list("circles" = s, "textspos" = p)
+    
+    if (plot == TRUE){
+        plotVenn(s, color = colors,
+         line  = line,lwd = lwd, frameadj = frameadj)
+        if (texts == TRUE){
+            plotVennTexts(p, Atext = names[1], Btext = names[2], Ctext = names[3],
+                          Value = Value, cex = cex, connect = connect, unit = unit)
+        }
+    }
+    return(out)
+}

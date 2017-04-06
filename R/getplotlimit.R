82f0d8ea374d6d6868f140d1c24bb1ea4b8cff5b
wildwolflin <weiting.lin.lin@gmail.com>
2017-04-06 11:57:13 -0500

    Edit getplot limit.R for C_all = NULL

--- R/getplotlimit.R
+++ R/getplotlimit.R
@@ -6,8 +6,18 @@ getPlotLimit <- function(vennout, adj = 1.5){
   r1 <- vennout$r1
   c2 <- vennout$c2
   r2 <- vennout$r2
-  c3 <- vennout$c3
-  r3 <- vennout$r3
+  
+  if ( ! "c3" %in% names(vennout)){
+    c3 <- c1 # use this dummy so the non-existing circle 3 will not affect limit
+    r3 <- r1
+      
+  } else {
+    
+    c3 <- vennout$c3
+    r3 <- vennout$r3
+  }
+  
+
   
   xmax <- max(c1[1] + r1, c2[1] + r2, c3[1] + r3)
   xmin <- min(c1[1] - r1, c2[1] - r2, c3[1] - r3)

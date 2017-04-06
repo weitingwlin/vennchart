179dc846288e2ab01250b5bbea095dbe97f56b72
wildwolflin <weiting.lin.lin@gmail.com>
2017-04-06 12:11:29 -0500

    Three circle works

--- R/plotvenntexts.R
+++ R/plotvenntexts.R
@@ -6,14 +6,18 @@ plotVennTexts <- function(P, Atext = "A", Btext = "B", Ctext = "C",
                           Value = TRUE, cex = 1, connect = "\n", unit = " ",
                           digits = 3){
     
+   
     # argument names overwrite the Atext, Btext,...
     if ( ! is.null(names)){
         Atext = names[1]
         Btext = names[2]
-        Ctext = names[3]
+        
+        if ("C" %in% names(P)){
+          Ctext = names[3]
+        }
     }
     
-    
+    # composing the text string
     if (Value == TRUE){
         At <- paste(Atext, connect, as.character(round(P$A, digits = digits)), unit)
         Bt <- paste(Btext, connect, as.character(round(P$B, digits = digits)), unit)
@@ -32,21 +36,24 @@ plotVennTexts <- function(P, Atext = "A", Btext = "B", Ctext = "C",
         BCt <- BCtext
     }
     
+  
     text(P$tpA[1], P$tpA[2], At, cex =cex, adj = c(0.5, 0.5))
     text(P$tpB[1], P$tpB[2], Bt, cex =cex, adj = c(0.5, 0.5))
-    text(P$tpC[1], P$tpC[2], Ct, cex =cex, adj = c(0.5, 0.5))
-    
     # only print if there is overlap
     if ( "tpAB" %in% names(P)){
         text(P$tpAB[1], P$tpAB[2], ABt, cex =cex, adj = c(0.5, 0.5))
     }
-    if ( "tpAC" %in% names(P)){
-            text(P$tpAC[1], P$tpAC[2], ACt, cex =cex, adj = c(0.5, 0.5))
-    }
-    if ( "tpBC" %in% names(P)){
-           text(P$tpBC[1], P$tpBC[2], BCt, cex =cex, adj = c(0.5, 0.5))
-    }
     
+    if ("C" %in% names(P)){
+        text(P$tpC[1], P$tpC[2], Ct, cex =cex, adj = c(0.5, 0.5))
+    
+        if ( "tpAC" %in% names(P)){
+            text(P$tpAC[1], P$tpAC[2], ACt, cex =cex, adj = c(0.5, 0.5))
+        }
+        if ( "tpBC" %in% names(P)){
+            text(P$tpBC[1], P$tpBC[2], BCt, cex =cex, adj = c(0.5, 0.5))
+        }
+    }
    
     
 }
